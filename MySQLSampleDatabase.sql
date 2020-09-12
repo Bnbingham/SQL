8327b3db1ac96ff88b014d7c4355d18f3e1e0194
Brad Bingham <bnbingham@hotmail.com>
2020-09-12 09:02:56 -0700

    started mysql proceedure

--- MySQLSampleDatabase.sql
+++ MySQLSampleDatabase.sql
@@ -12,10 +12,30 @@ CREATE TABLE login_info(
     username varchar(30) UNIQUE NOT NULL,
     password varchar(30) NOT NULL
 );
+
+
+
+
+
+
+
+------------------------------------------------------------------------------------------------------------------------
 insert into customer values (NULL,'Brad','Bingham',NULL);
 select * from customer;
 
+SHOW TABLE STATUS FROM `mysqldb` WHERE `name` LIKE 'customer';
 
+SELECT `AUTO_INCREMENT`
+FROM  INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'mysqldb'
+AND   TABLE_NAME   = 'customer';
 
+SELECT last_insert_id();
 
-
+/*
+CREATE PROCEDURE `new_procedure` (fName varchar(30), lName varchar(30),email varchar(50),username varchar(30),password varchar(30))
+BEGIN
+	insert into customer values(NULL,fName,lName,email);
+	insert into login_info values(last_insert_id(),username, password);
+END 
+*/
