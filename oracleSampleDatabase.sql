6ee8ce9e009fb6deac07ddefc3a794494d66260b
Brad Bingham <bnbingham@hotmail.com>
2020-09-15 14:05:10 -0700

    updated Oracle SQL

--- oracleSampleDatabase.sql
+++ oracleSampleDatabase.sql
@@ -1,4 +1,4 @@
---User and grant privilages
+-----------------------------User and grant privilages-----------------------------
 CREATE USER sampleDatabase
     IDENTIFIED BY Pa55word
     DEFAULT TABLESPACE users
@@ -12,8 +12,14 @@ GRANT create table TO sampleDatabase;
 GRANT create view TO sampleDatabase;
 
 conn sampleDatabase/Pa55word
-----------------------------
---Tables
+
+-----------------------------Tables-----------------------------
+exec drop_table_if_exists('login_info');
+exec drop_table_if_exists('purchased_items');
+exec drop_table_if_exists('purchase');
+exec drop_table_if_exists('customer');
+exec drop_table_if_exists('item');
+
 --Customer/Login_info are seperate to demonstrate how to work with a one-to-one relationship
 CREATE TABLE customer(
     id number GENERATED AS IDENTITY PRIMARY KEY,
@@ -33,26 +39,33 @@ CREATE TABLE login_info(
 CREATE TABLE purchase(
     id NUMBER GENERATED AS IDENTITY PRIMARY KEY,
     customerID REFERENCES customer(id) ON DELETE SET NULL,
-    date_loaded TIMESTAMP(6) WITH TIME ZONE
+    date_loaded TIMESTAMP(2) WITH TIME ZONE 
 );
 CREATE TABLE item(
     id number GENERATED AS IDENTITY PRIMARY KEY,
-    product varchar2(30),
-    price number(*,2)
+    product varchar2(30) UNIQUE NOT NULL,
+    price number(*,2) NOT NULL
 );
 CREATE TABLE purchased_items(
-    purchaseID number REFERENCES purchase (id),
-    itemID number REFERENCES item (id),
+    purchaseID number REFERENCES purchase (id) NOT NULL,
+    itemID number REFERENCES item (id) NOT NULL,
     quantity number NOT NULL,
     PRIMARY KEY(purchaseID,itemID)
 );
 
---Views
+-----------------------------Views-----------------------------
 CREATE OR REPLACE VIEW customers 
 AS 
     SELECT customer.id, customer.first_name, customer.last_name,customer.email,login_info.username,login_info.password
     FROM customer
     INNER JOIN login_info ON customer.id = login_info.id;
+
+CREATE OR REPLACE VIEW sold_items
+AS
+    SELECT * 
+    FROM purchased_items 
+    INNER JOIN item 
+    ON purchaseID = purchased_items.itemid;
 
 CREATE OR REPLACE VIEW purchases
 AS
@@ -60,13 +73,13 @@ AS
         purchase.id, 
         (customer.first_name||' '||customer.last_name) AS customer_name,
         to_char(purchase.date_loaded, 'DD-Mon-YYYY') AS purchase_date,
-        (SELECT COUNT(*) FROM purchased_items WHERE purchaseID=purchase.id) as unique_items,
-        (SELECT SUM(quantity) FROM purchased_items INNER JOIN item ON itemID = id WHERE purchaseID=purchase.id ) as total_items,
-        (SELECT SUM(quantity * price) FROM purchased_items INNER JOIN item ON itemID = id WHERE purchaseID=purchase.id ) as total_price
+        (SELECT COUNT(*) FROM sold_items WHERE purchaseID=purchase.id) as unique_items,
+        (SELECT SUM(quantity) FROM sold_items WHERE purchaseID=purchase.id ) as total_items,
+        (SELECT SUM(quantity * price) FROM sold_items WHERE purchaseID=purchase.id ) as total_price
     FROM purchase
     INNER JOIN customer ON purchase.customerid = customer.id;
 
---Procedures
+-----------------------------Procedures-----------------------------
 CREATE OR REPLACE PROCEDURE add_customer
 (fName varchar2, lName varchar2,email varchar2, username varchar2, password varchar2)
 AS
@@ -81,12 +94,32 @@ BEGIN
 END;
 /
 
+-- '-942' is the exception code specifically for "table not found"
+CREATE OR REPLACE PROCEDURE drop_table_if_exists
+(table_name varchar2)
+AS
+BEGIN
+   EXECUTE IMMEDIATE 'DROP TABLE ' || table_name;
+EXCEPTION
+   WHEN OTHERS THEN
+      IF SQLCODE != -942 THEN 
+         RAISE;
+      END IF;
+END;
+/
 
 
--------------------------------------------------------------------------------------------------------
---need to make
---function
---something that uses cursors
---trigger
---%TYPE attribute
+
+----------------------------------------NOTES---------------------------------------------------
+exec add_customer('Brad','Bing','email@emial.com','brad1','pass1');
+select * from customers;
+insert into purchase(customerID, date_loaded) 
+    values(1,CURRENT_TIMESTAMP);
+select * from purchase;
+insert into item(product, price) values ('banana',.25);
+insert into item(product, price) values ('apple',.35);
+select * from item;
+insert into purchased_items values(1,1,2);
+insert into purchased_items values(1,2,3);
+
 
